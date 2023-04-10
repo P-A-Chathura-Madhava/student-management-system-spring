ee7974758d5237628d8874478649ced3f04cc139
P-A-CHATHURA-MADHAVA <ctechict101@gmail.com>
2023-04-10 12:28:59 +0530

    Complete saveStudent

--- src/main/java/lk/ijse/dep9/controller/StudentController.java
+++ src/main/java/lk/ijse/dep9/controller/StudentController.java
@@ -1,4 +1,51 @@
 package lk.ijse.dep9.controller;
 
+import lk.ijse.dep9.dto.ResponseDTO;
+import lk.ijse.dep9.dto.StudentDTO;
+import lk.ijse.dep9.service.StudentService;
+import lk.ijse.dep9.util.VarList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/student")
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @Autowired
+    private ResponseDTO responseDTO;
+
+    @PostMapping(value = "/saveStudent")
+    public ResponseEntity saveEmployee(@RequestBody StudentDTO studentDTO){
+        try {
+            String res=studentService.saveStudent(studentDTO);
+            if (res.equals("00")){
+                responseDTO.setCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Success");
+                responseDTO.setContent(studentDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+            }else if(res.equals("06")) {
+                responseDTO.setCode(VarList.RSP_DUPLICATED);
+                responseDTO.setMessage("Employee Registered");
+                responseDTO.setContent(studentDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            }else {
+                responseDTO.setCode(VarList.RSP_FAIL);
+                responseDTO.setMessage("Error");
+                responseDTO.setContent(null);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception ex){
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(ex.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
